d099130ee95af8b3aeeab8c544906fa5a306c957
QuinntyneBrown <quinntynebrown@gmail.com>
2021-01-18 17:54:18 -0500

    update

---
+++ src/AspNetCoreContentful.Api/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreContentful.Api.Models;
+using Contentful.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetCoreContentful.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController
+    {
+        private readonly IContentfulClient _client;
+        public CourseController(IContentfulClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> Get()
+        {
+            var courses = await _client.GetEntries<Course>();
+
+            return courses;
+        }
+    }
+}
---
+++ src/AspNetCoreContentful.Api/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreContentful.Api.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+    }
+}
